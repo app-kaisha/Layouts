f5d816eb3e8fdd03bf959d26e217aaf69cb03ab9
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-29 20:42:04 +0000

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,18 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by app-kaihatsusha on 29/12/2025.
+//  Copyright © 2025 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
