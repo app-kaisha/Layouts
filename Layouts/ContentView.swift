9c560fb803b5a71491fe2c7d86afb69401dfee2e
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-30 12:01:36 +0000

    Ch1.3 Layout - Geometry reading and flooding safe ares with colour

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -14,37 +14,61 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            
-            Spacer()
-            
-            Text(message)
-            //                .border(.blue)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.goldBC)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-            //                .border(.orange, width: 2)
-                .padding()
-            
-            Spacer()
-            
-            HStack {
-                Button("Awesome") {
-                    message = "You Are Awesome!"
-                }
+        GeometryReader { geometry in
+            VStack {
+                Text("Awesome!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.indigo)
+                
                 Spacer()
-                Button("Great") {
-                    message = "You Are Great!"
+                
+                Text(message)
+                //                .border(.blue)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                //                .border(.orange, width: 2)
+                    .padding()
+                
+                Spacer()
+                
+                Divider()
+                    .background(.black)
+                    .frame(width: geometry.size.width * 2/3)
+                    .padding()
+                    
+                
+                Rectangle()
+                    .fill(.indigo)
+                    .frame(width: geometry.size.width * 2/3, height: 2)
+                
+                
+                HStack {
+                    Button("Awesome") {
+                        message = "You Are Awesome!"
+                    }
+                    Spacer()
+                    
+                    Button("Great") {
+                        message = "You Are Great!"
+                    }
                 }
+                .buttonStyle(.borderedProminent)
+                //            .border(.purple, width: 5)
+                .padding()
+                
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.indigo)
             }
-            .buttonStyle(.borderedProminent)
-            //            .border(.purple, width: 5)
-            .padding()
-            
         }
         
     }
